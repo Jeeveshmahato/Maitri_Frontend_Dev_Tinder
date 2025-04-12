935f01f562d06bc930480e14d74c9a4e3917388b
Jeeveshmahato <jeeveshmahato100@gmail.com>
2025-04-12 13:38:17 +0530

    updates

--- src/Utiles/Constants.js
+++ src/Utiles/Constants.js
@@ -1,2 +1,4 @@
 // export const BaseUrl = "http://localhost:5000"
-export const BaseUrl = "/api"
+export const BaseUrl =
+  location.hostname === "localhost" ? "http://localhost:5000" : "/api";
+// export const BaseUrl = "/api"
